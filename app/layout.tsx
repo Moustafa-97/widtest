918981e777ef70cccf22c6156d01d5a5e81a6505
Moustafa <moustafaadel097@gmail.com>
2024-11-12 03:39:47 +0200

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { Metadata } from "next";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
-import "./[locale]/globals.css";
-
-// import Script from "next/script";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "WID not found",
@@ -18,17 +14,12 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const messages = await getMessages(locale as any);
-
   return (
     <html lang={locale}>
       <body>
-        <NextIntlClientProvider messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-
-        {/* <Script src="https://secure-egypt.PayTabs.com/payment/js/paylib.js" /> */}
+        {children}
+        <Script src="https://secure-egypt.PayTabs.com/payment/js/paylib.js" />
       </body>
-    </html>
+     </html>
   );
 }
