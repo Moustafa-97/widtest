14a512450a2239002c4610235658b32bcde0ebe1
Moustafa <moustafaadel097@gmail.com>
2025-01-13 17:32:30 +0200

    updates

--- app/[locale]/privacyAndPolicy/page.tsx
+++ app/[locale]/privacyAndPolicy/page.tsx
@@ -1,10 +1,10 @@
-import TermsOfService from "@/Components/privacy/Privacy";
+import TermsOfService from "@/Components/privacy/Terms";
 import React from "react";
 
 function page() {
   return (
     <>
-      <section style={{margin:"150px auto auto auto"}}>
+      <section style={{ margin: "150px auto auto auto" }}>
         <TermsOfService />
       </section>
     </>
