4deddc4211010adf604d45db04d0becad26f6696
Moustafa <moustafaadel097@gmail.com>
2025-01-28 10:02:52 +0200

    update

--- Utils/aos/aos.tsx
+++ Utils/aos/aos.tsx
@@ -1,16 +1,16 @@
-"use client";
+// "use client";
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
 
-export const AOSInit = (time: number) => {
-  if (typeof window !== "undefined") {
-    AOS.init({
-      easing: "ease-in-out",
-      duration: time,
-      once: false,
-    });
-  }
+// export const AOSInit = (time: number) => {
+//   if (typeof window !== "undefined") {
+//     AOS.init({
+//       easing: "ease-in-out",
+//       duration: time,
+//       once: false,
+//     });
+//   }
 
-  return null;
-};
+//   return null;
+// };
